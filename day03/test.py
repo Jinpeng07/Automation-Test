a8cdbbf01a24901162ad81062f4673845a18df3a
Jinpeng <1832508189@qq.com>
2023-06-24 17:20:37 +0800

    day3-2

---
+++ day03/test.py
@@ -0,0 +1,19 @@
+# print(10 | 2)
+#
+# a = 10
+# print(a >> 2)
+#
+# print('zhu' not in 'zhuzhu')
+list1 = ['a', 'b', 'c']
+list2 = ['a', 'b', 'c']
+s1 = 'abc'
+s2 = 'abc'
+print(list1 is list2)
+print(s1 is s2)
+#
+# print(isinstance('1', int))
+#
+# a = 1 if True else 2
+# print(a)
+# a = 10
+# print(~a)

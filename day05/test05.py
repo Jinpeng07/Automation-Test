eb6c3ed160dbd061dda0cd83f6914ba9144365b3
Jinpeng <1832508189@qq.com>
2023-06-26 00:25:51 +0800

    day5

--- day05/test05.py
+++ day05/test05.py
@@ -24,9 +24,27 @@
 # random.shuffle(mylist)
 # print(mylist)
 
-mylist = [3,421,4,54,23,3]
-for i in range(len(mylist)):
-    for j in range(len(mylist) - 1 -i):
-        if mylist[j] < mylist[j+1]:
-            mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
-print(mylist)
+# mylist = [3, 421, 4, 54, 23, 3]
+# for i in range(len(mylist)):
+#     for j in range(len(mylist) - 1 - i):
+#         if mylist[j] < mylist[j + 1]:
+#             mylist[j], mylist[j + 1] = mylist[j + 1], mylist[j]
+# print(mylist)
+#
+# list2 = [1, 2, 3, [1, 2, 34, 4]]
+# tup = tuple(list2)
+# print(tup)
+# tup[3][0] = 100
+# print(tup)
+import random
+set1=set()
+for i in range(20):
+    n=random.randint(1,100)
+    set1.add(n)
+
+print(set1)
+
+for i in range(len(set1)-1):
+    set1.pop()
+
+print(set1)
